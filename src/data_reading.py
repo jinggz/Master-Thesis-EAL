e7fe65a4e92dd8fe772e1698f3b230e327fb9164
Jingyi <jingyi.zhang@sap.com>
2018-10-21 22:47:11 +0200

    solved file path

--- src/data_reading.py
+++ src/data_reading.py
@@ -1,9 +1,12 @@
-import os.path
+
+from pathlib import Path
+
 
 def read_raw_data(file):
+    # read the subjlink.txt file and store the needed tuples into dict
     res ={}
-    # TODO use proper file path pacakge
-    with open(__dir__(file), 'r', encoding='utf-8') as f:
+
+    with open(file, 'r', encoding='utf-8') as f:
          for line in f:
              # TODO split line
              print(line)
@@ -12,4 +15,5 @@ def read_raw_data(file):
 if __name__ == '__main__':
 
     input_file = "data/subjlink.txt"
-    read_raw_data(input_file)
+    input_dir = Path.joinpath(Path(__file__).parent.parent , input_file)
+    read_raw_data(input_dir)
