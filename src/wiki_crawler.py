8e5afa540c80d56c16870161949adf61e99438fe
Jingyi <jingyi.zhang@sap.com>
2018-11-18 23:12:52 +0100

    add function: extract url and bug fixed

--- src/wiki_crawler.py
+++ src/wiki_crawler.py
@@ -2,10 +2,12 @@ import pickle
 from pathlib import Path
 import bs4 as bs
 import urllib.request
+from urllib.parse import quote
 import time
 import pprint as pp
 import logging
 import json
+import re
 
 from data_reader import DataReader
 
@@ -21,18 +23,26 @@ class EntityPage:
         self.page_dict = dict()
         self.soup = None
 
+    def get_connection(self, entity):
+        try:
+            # example: https://en.wikipedia.org/wiki/Segal%E2%80%93Bargmann_space
+            connection = urllib.request.urlopen('https://en.wikipedia.org/wiki/' + quote(entity))
+        except Exception as e:
+            logger.exception(e, exc_info=False)
+            return []
+        return connection
+
     def retrieve_wiki_page(self, entity):
         # To connect and extract the page content of the Wiki Page of the entity
-        err_log = ''
-        try:
-            source = urllib.request.urlopen('https://en.wikipedia.org/wiki/' + entity).read()
+        connection = self.get_connection(entity)
+        if connection:
+            source = connection.read()
             soup = bs.BeautifulSoup(source, 'lxml')
-        except Exception as exception:
-            err_log = "No page of [%s]: [%s] %s" % (entity, exception, exception.url)
-            soup = []
-
+        # remove unused tags
+        [x.extract() for x in soup.find_all('sup')]
+        [x.extract() for x in soup.find_all(class_='mw-editsection')]
+        [x.extract() for x in soup.find_all(class_='image')]
         self.soup = soup
-        return err_log
 
     def get_page_dict_4_entity(self):
         h2_list = self.get_h2_list()
@@ -43,8 +53,8 @@ class EntityPage:
                 mark = self.soup.find(id=h2_id)
                 content = self.get_text_bw_2h2(mark)
                 self.page_dict[h2_str] = content
-            except Exception as exception:
-                logger.info(exception)
+            except Exception as e:
+                logger.info(e)
                 pass
 
     #  TODO collect contents b4 the first h2 -- summary
@@ -60,52 +70,59 @@ class EntityPage:
         :param: mark: the start section name. typ: str
         :return: the whole text between 2 sections as a string
         '''
-        result = []
+        contents = []
+        links = []
         for elt in mark.parent.next_siblings:
             if elt.name == "h2":
                 break
             try:
-                for chi in elt.children:
-                    if chi.name == 'sup':
-                        continue
-                    else:
-                        result.append(str(chi))
-            except (AttributeError, KeyError):
-                pass
-        result = " ".join(result)
-        return result
+                for link in elt.find_all('a', href=self.__not_file):
+                    links.append(urllib.parse.unquote((link.get('href'))))
+                contents.append(elt.get_text(' ', strip=True))
+            except Exception as e:
+                logger.exception(e, exc_info=False)
+        links = " ".join(links)
+        contents = " ".join(contents)
+        return contents, links
+
+    def __not_file(self, href):
+        return href and not re.compile("File").search(href)
+
 
 if __name__ == '__main__':
-    dir = Path(__file__).parent.parent
 
-    input_file = Path.joinpath(dir, 'data/data_eal.json')
-    output_file = 'data/wiki.pickle'
+    dir = Path(__file__).parent.parent
+    input_file = Path.joinpath(dir, 'output/sentences_eal_subj.json')
+    #output_file = Path.joinpath(dir, 'output/entity_dict/')
 
     # load the data
     with open(input_file, 'r', encoding='utf-8') as f:
-        data = json.load(f)
+        data = json.load(f, encoding='utf-8')
     logger.info('Sentence data loaded')
 
+    # load list of entities
     entities = set(row['entity'] for row in data)
     entities.discard('None')
     # 19985 entities, among them 10410 unique entities
     logger.info('Remove empty entity')
-    # TODO remove entity_sect_text_dict
+
+    # begin crawling
     entity_sect_text_dict = dict()
     logger.info('Start to create dictionary for Wiki page... ')
     for entity in entities:
         instance = EntityPage()
-        err = instance.retrieve_wiki_page(entity)
-        if err:
-            logger.info(err)
-        else:
-            instance.get_page_dict_4_entity()
-            entity_sect_text_dict[entity] = instance.page_dict
-            # TODO save to seperate json file
-            # logger.info('Save the dictionary of %s to %s.' % (entity, output_file) )
+        instance.retrieve_wiki_page(entity)
+        instance.get_page_dict_4_entity()
+        #TODO save to seperate json file
+        #         # #saver.save2json(file, instance.page_dict)
+        #entity_sect_text_dict[entity] = instance.page_dict
+
+    #         #logger.info('Saved the dictionary of %s to %s.' % (entity, output_file) )
+    logger.info('Finished dictionary creation.')
 
     # pp.pprint(entity_sect_text_dict['Cello Suites (Bach)'])
     # saver = DataReader()
-    # saver.save2pickle(output_file)
-    # TODO save each entity page in a seperate file
+    # saver.save2json(Path.joinpath(dir, 'output/entity_dicts.json'), entity_sect_text_dict)
+    # logger.info('Saved the dictionary of entities to file.')
+
 
