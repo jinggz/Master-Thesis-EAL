53611a64b90794b6386cb395537b06f77c1c1d4d
Jingyi <jingyi.zhang@sap.com>
2018-12-30 14:32:30 +0100

    filter sentence samples

---
+++ src/filter_sentences.py
@@ -0,0 +1,89 @@
+# This file is used to filter out unwanted sentences samples,
+# and store wanted samples into sentences_clean_xxx.json for furture training
+# The following samples will be excluded or modified:
+    # no corresponding entity page found in our wiki corpus
+    # entity name starting with “List of” or “Lists of", or containing “(disambiguation)”
+    # no corresponding aspect found in our wiki corpus
+    # for aspects found as lower level of headings, replace with h2 headings
+    # remove duplicates
+# reference: http://trec-car.cs.unh.edu/process/dataselection.html
+import os
+import json
+import logging
+from pathlib import Path
+import pandas as pd
+import ahocorasick
+import set_env
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger.setLevel(logging.INFO)
+
+def filter_samples(wiki_dict, df):
+    '''
+    :param
+    wiki_dict: the dictionary of entity-aspects pages
+    :type: dict
+    sentences: the raw sentence tuples extracted from Kiril's data
+    :type: dataframe
+    :return: sentences: the filtered sentences samples for training
+    :type: dataframe
+    '''
+
+    df.drop(columns=['factuality', 'triple'], inplace=True) # only use column 'sentence', 'entity', 'aspect'
+    df.drop_duplicates(inplace=True)
+    df.drop(df[(df.entity == 'None') | (df.aspect == 'None')].index, inplace=True)
+    df.index = range(len(df.index))
+    df = df.apply(lambda x: x.str.lower())
+
+    lst = ['none', 'list of', 'lists of', 'disambiguation', 'category', 'categories', 'gaa', 'cup', 'season',
+           'champions', 'league']
+    df = df[using_ahocorasick(df.entity, lst) == False] # filter out entity contain substr in lst
+    df.index = range(len(df.index))
+
+    for row in df.itertuples():
+        aspects_dict = wiki_dict.get(row.entity)
+        if aspects_dict is None:
+            df.drop(row.Index, inplace=True)
+        elif row.aspect not in aspects_dict:
+            for k in aspects_dict:
+                if aspects_dict[k].get('heads') and row.aspect in aspects_dict[k].get('heads'):
+                    df.at[row.Index, 'aspect'] = k
+                    break
+            else:
+                df.drop(row.Index, inplace=True)
+
+        df.index = range(len(df.index))
+
+    logger.info('The total number of used samples: %s' % len(df))
+    return df
+
+def using_ahocorasick(col, lst):
+    A = ahocorasick.Automaton(ahocorasick.STORE_INTS)
+    for word in lst:
+        A.add_word(word.lower())
+    A.make_automaton()
+    col = col.str.lower()
+    col = col.str.replace('_', ' ')
+    mask = col.apply(lambda x: bool(list(A.iter(x))))
+    return mask
+
+if __name__ == '__main__':
+
+    dir = Path(__file__).parent.parent
+    wiki_file = Path.joinpath(dir, os.getenv('wiki_file'))
+    sentence_file = Path.joinpath(dir, os.getenv('sentence_file'))
+    out_file = Path.joinpath(dir, 'trained', 'sentences_clean_subj.json')
+    with open(wiki_file, 'r', encoding='utf-8') as f:
+        wiki_dict = json.load(f)
+    sentences = pd.read_json(path_or_buf=sentence_file,
+                      orient='records')
+    logger.info('Original files loaded')
+
+    clean_sentences = filter_samples(wiki_dict, sentences)
+    # no need to save, can be used as the next input of tfidf_ranking.py
+    # with open(out_file, 'w', encoding='utf-8') as f:
+    #     json.dump(clean_sentences, f)
+    # logger.info('Stored the cleaned sentences file to %s' % out_file)
