e37f8d93036e983474cbf3accba483531e7e5b99
Jingyi <jingyi.zhang@sap.com>
2019-01-01 11:42:44 +0100

    tfidf model training

--- src/tfidf.py
+++ src/tfidf.py
@@ -1,9 +1,6 @@
 import logging
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
 # from nltk.corpus import stopwords
-from data_reader import DataReader
 from pathlib import Path
 import pickle
 import json
@@ -17,66 +14,51 @@ logger.setLevel(logging.INFO)
 
 class Tfidf:
 
-    def train(self, corpus, max_features=20000):
+    def __init__(self, logger):
+        self.vectorizer = TfidfVectorizer(norm='l2', use_idf=True, smooth_idf=True,
+                                          sublinear_tf=True, analyzer='word',
+                                          decode_error='ignore')
+        self.logger = logger
+
+    def train(self, docs):
         '''
         use training corpus to train a tf-idf model and save
-        :param corpus: type: list of string
-        :return:
+        :param docs: type: list of string
+        :return:tfidf model after fit,
+        :type: TfidfVectorizer object
         '''
-        tfidf_model = TfidfVectorizer(norm='l2', use_idf=True, smooth_idf=True,
-                                          sublinear_tf=True, analyzer='word', max_features=max_features,
-                                          decode_error='ignore')
-        idfs = tfidf_model.fit(corpus)
+        self.vectorizer.fit(docs)
         logger.info('finished training tfidf model')
+
+    def save_tfidf_model(self, model_path):
         # save model
-        reader = DataReader()
-        dir = reader.get_file_dir()
-        with open(Path.joinpath(dir, 'output/tfidf_model_content.pkl'), 'wb') as f:
-            pickle.dump(tfidf_model, f)
-        with open(Path.joinpath(dir, 'output/tfidf_idfs_content.pkl'), "wb") as f:
-            pickle.dump(idfs, f)
-        logger.info('tfidf model saved')
+        with open(model_path, 'wb') as f:
+            pickle.dump(self.vectorizer, f)
 
-    def load_corpus(self):
-        reader = DataReader()
-        dir = reader.get_file_dir()
-        with open(Path.joinpath(dir, 'output/wiki.json'), 'r', encoding='utf-8') as f:
-            data = json.load(f)
-        return data
-
-    def build_corpus(self):
-        # each aspect is regarded as a doc
-        corpus = []
-        data = self.load_corpus()
-        for entity in data.values():
-            for section in entity:
-                if section != 'lead_paragraphs':
-                    corpus.append(entity[section]['content'])
-        return corpus
-
-    def load_tfidf_model(self):
-        reader = DataReader()
-        dir = reader.get_file_dir()
-        with open(Path.joinpath(dir, 'output/tfidf_model_content.pkl'), 'rb') as f:
-            tfidf_model = pickle.load(f)
-        with open(Path.joinpath(dir, 'output/tfidf_idfs_content.pkl'), "rb") as f:
-            idfs = pickle.load(f)
-        return tfidf_model, idfs
-
-    def get_tfidf(self, doc):
-        # Create new tfidfVectorizer with old vocabulary
-        tfidf_model, idfs = self.load_tfidf_model
-        # TODO cant transform directly
-        X_vector = tfidf_model.transform(doc)
-        return X_vector
-
-    def cos_sim(self, a, b):
-        return cosine_similarity(a, b).flatten()
+def retrieve_docs(data_path):
+    # each aspect is regarded as a doc
+    logger.info("Starting docs retrieval")
+    docs = list()
+    with open(data_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    for entity in data.values():
+        for section in entity:
+            if section != 'lead_paragraphs':
+                doc = entity[section]['content'].lower()
+                docs.append(doc)
+    return docs
 
 if __name__ == '__main__':
-    # one time execution
-    tfidf = Tfidf
-    corpus = tfidf.build_corpus()
-    tfidf.train(corpus)
 
-    # TODO cosine similarity between vector of content and vector of sentence
+    dir = Path(__file__).parent.parent
+
+    docs = retrieve_docs(Path.joinpath(dir, 'trained', 'wiki_subj.json'))
+
+    logger.info('Starting tfidf training for wikipedia corpus')
+    tfidf_trainer = Tfidf(logger)
+    tfidf_trainer.train(docs)
+    # save to file
+    tfidf_trainer.save_tfidf_model(Path.joinpath(dir, 'model', 'tfidf_subj.pkl'))
+    logger.info('Saved tf-idf trained model')
+
+
