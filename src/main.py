ca595624e4b5c81900403ee5fea837e01044342b
Jingyi <jingyi.zhang@sap.com>
2019-01-06 12:37:32 +0100

    main

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import os
+import logging
+from pathlib import Path
+from tfidf_ranking import EAL
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format='%(asctime)s %(name)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger.setLevel(logging.INFO)
+
+
+dir = Path(__file__).parent.parent
+# set model file to your local path
+model_file = Path.joinpath(dir, 'model', 'tfidf_model.pkl')
+
+while True:
+    sentence = input('enter sentence:')
+    entity = input('enter entity: ')
+    ranking = EAL(model_file)
+    aspect_predicted = ranking.get_prediction(sentence, entity)
+    print('Predicted most relevant aspect is: %s' % aspect_predicted)
+
+
+
+
+
